ef757e3ae46e05d78c84ea995842a5619a0aaeed
Agnes Selvita <a.selvita@gmail.com>
2020-09-17 12:22:47 +0700

    first commit

---
+++ files/base_controller.rb
@@ -0,0 +1,90 @@
+class Api::V1::BaseController < ActionController::Base
+  # protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
+  before_action :set_locale
+  before_action :authenticate_user!
+
+  APP_ID = Rails.application.credentials.dig(:wechat, :app_id)
+  APP_SECRET = Rails.application.credentials.dig(:wechat, :app_secret)
+
+  def wx_fetch_token
+    app_id = APP_ID
+    app_secret = APP_SECRET
+    url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{app_id}&secret=#{app_secret}"
+
+    @wechat_response ||= RestClient.get(url)
+    JSON.parse(@wechat_response.body).fetch("access_token")
+  end
+
+  def render_error(object)
+    render json: { status: 'fail', res: 'fail', errors: object.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def check_admin_permission
+    unless current_user.is_admin
+      render json: {
+        errors: "User has no admin authorization."
+      }
+    end
+  end
+
+  def decrypt(user, data, iv)
+    sesskey = Base64.decode64(user.session_key)
+    data = Base64.decode64(data)
+    iv = Base64.decode64(iv)
+
+    decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    decipher.decrypt
+    decipher.key = sesskey
+    decipher.iv = iv
+
+    plain = decipher.update(data) + decipher.final
+    puts plain
+    JSON.parse(plain)
+  end
+
+  private
+
+  def check_headers
+    puts response.headers
+  end
+
+  def set_locale
+    # I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym || "en".to_sym
+    I18n.locale = params[:locale].present? ? params[:locale].to_sym : :en
+  end
+
+  def not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def internal_server_error(exception)
+    if Rails.env.development?
+      response = { type: exception.class.to_s, error: exception.message }
+    else
+      response = { error: "Internal Server Error" }
+    end
+    render json: response, status: :internal_server_error
+  end
+
+  def tl
+    I18n.t('mp')
+  end
+
+  def blob_link(x)
+    host = HOST
+    Rails.application.routes.url_helpers.rails_blob_url(x, host: host)
+  end
+
+  # def active_tab
+  #   # Overwrite method in each individual controller.
+  #   # Let's say in controller for 2nd tab:
+  #   # ex:  return 1
+  #   #
+  #   return 0
+  # end
+
+  # Intelligent Model-Fetching from Kablam codebase.
+  # table-name should be the params in url.
+
+end
---
+++ wxmp.rb
@@ -0,0 +1,309 @@
+run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
+
+# GEMFILE
+########################################
+inject_into_file 'Gemfile', before: 'group :development, :test do' do
+  <<~RUBY
+    # gem 'sidekiq', '~> 6.1'
+    gem 'devise'
+    gem 'tiddle'
+    gem 'active_model_serializers', '~> 0.10.0'
+    gem 'activestorage-aliyun'
+    gem 'rest-client'
+    gem 'rails-i18n'
+
+    # for rails admin
+    # gem 'mini_magick'
+    # gem 'rails_admin'
+
+    # gem 'rqrcode' # to generate basic QR code
+    # gem 'aliyun-sms' # to send SMS via Aliyun SMS
+
+    # CHINA payments
+    # gem 'wx_pay'
+    # gem 'alipay', '~> 0.15.1'
+
+    # fix some lib-error
+    gem 'rb-readline'
+    gem 'dotenv-rails'
+
+  RUBY
+end
+
+inject_into_file 'Gemfile', after: 'group :development, :test do' do
+  <<-RUBY
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  RUBY
+end
+
+# gsub_file('Gemfile', /# gem 'redis'/, "gem 'redis'")
+
+# Assets
+########################################
+# run 'rm -rf app/assets/stylesheets'
+# run 'rm -rf vendor'
+# run 'curl -L https://github.com/lewagon/stylesheets/archive/master.zip > stylesheets.zip'
+# run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets'
+
+# Dev environment
+########################################
+# gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
+gsub_file('config/environments/development.rb', /config\.active_storage\.service.*/, 'config.active_storage.service = :aliyun')
+
+# Production environment
+########################################
+gsub_file('config/environments/production.rb', /config\.active_storage\.service.*/, 'config.active_storage.service = :aliyun')
+
+# Staging environment
+########################################
+run 'cp config/environments/production.rb config/environments/staging.rb'
+
+# Layout
+########################################
+# NEED TO CHECK
+if Rails.version < "6"
+  scripts = <<~HTML
+    <%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>
+        <%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>
+  HTML
+  gsub_file('app/views/layouts/application.html.erb', "<%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload' %>", scripts)
+end
+gsub_file('app/views/layouts/application.html.erb', "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>", "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>")
+style = <<~HTML
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>
+HTML
+gsub_file('app/views/layouts/application.html.erb', "<%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>", style)
+
+# Flashes
+########################################
+# file 'app/views/shared/_flashes.html.erb', <<~HTML
+#   <% if notice %>
+#     <div class="alert alert-info alert-dismissible fade show m-1" role="alert">
+#       <%= notice %>
+#       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+#         <span aria-hidden="true">&times;</span>
+#       </button>
+#     </div>
+#   <% end %>
+#   <% if alert %>
+#     <div class="alert alert-warning alert-dismissible fade show m-1" role="alert">
+#       <%= alert %>
+#       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+#         <span aria-hidden="true">&times;</span>
+#       </button>
+#     </div>
+#   <% end %>
+# HTML
+
+# run 'curl -L https://github.com/lewagon/awesome-navbars/raw/master/templates/_navbar_wagon.html.erb > app/views/shared/_navbar.html.erb'
+
+# inject_into_file 'app/views/layouts/application.html.erb', after: '<body>' do
+#   <<-HTML
+
+#     <%= render 'shared/navbar' %>
+#     <%= render 'shared/flashes' %>
+#   HTML
+# end
+
+# README
+########################################
+# markdown_file_content = <<-MARKDOWN
+# Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+# MARKDOWN
+# file 'README.md', markdown_file_content, force: true
+
+# Generators
+########################################
+generators = <<~RUBY
+  config.generators do |generate|
+    generate.assets false
+    generate.helper false
+    generate.test_framework :test_unit, fixture: false
+  end
+RUBY
+
+environment generators
+
+########################################
+# AFTER BUNDLE
+########################################
+# 1. Devise + Tiddle
+# 2. Active Storage config
+# 3. User serializer
+
+after_bundle do
+  # Generators: db + simple form + pages controller
+  ########################################
+  rails_command 'db:drop db:create db:migrate'
+  # generate('simple_form:install', '--bootstrap')
+  # generate(:controller, 'pages', 'home', '--skip-routes', '--no-test-framework')
+
+  # Routes
+  ########################################
+  # route "root to: 'pages#home'"
+  route <<~RUBY
+    scope "(:locale)", locale: /en|cn/ do
+
+      namespace :api, defaults: { format: :json } do
+        namespace :v1 do
+
+          # Devise routes for API clients (custom sessions controller)
+          devise_scope :user do
+            post 'login', to: 'user_sessions#create', as: 'login'
+            post 'manual_login', to: "user_sessions#manual_create"
+          end
+
+          get 'users/:id', to: "users#show"
+          put 'users/update', to: 'users#update'
+          # put 'users/update-phone', to: 'user_sessions#update_phone'
+          # put 'users/admin-register', to: 'user_sessions#admin_register'
+        end
+      end
+
+  end
+  RUBY
+
+  # Git ignore
+  ########################################
+  append_file '.gitignore', <<~TXT
+    # Ignore .env file containing credentials.
+    .env*
+    # Ignore Mac and Linux file system files
+    *.swp
+    .DS_Store
+  TXT
+
+  # Devise install + user
+  ########################################
+  generate('devise:install')
+  generate('devise', 'User')
+
+  # Tiddle
+  ########################################
+  generate(:model, 'AuthenticationToken', 'body:string', 'user:references', 'last_used_at:datetime', 'ip_address:string', 'user_agent:string')
+  # add :token_authenticatable
+  # replace devise :database_authenticatable, :registerable, :recoverable, :trackable, :validatable
+  # with devise :database_authenticatable, :registerable, :recoverable, :trackable, :validatable, :token_authenticatable
+  inject_into_file 'app/models/user.rb', after: ":validatable" do
+    ", :token_authenticatable"
+  end
+
+  inject_into_file 'app/models/user.rb', gefore: "end" do
+    "has_many :authentication_tokens"
+  end
+
+  # WeChat User Attributes
+  ########################################
+  generate('migration AddBasicColumnsToUsers')
+  dir = 'db/migrate'
+  migration_files_array = Dir.entries(dir)
+  file = migration_files_array.find {|f| f.match('add_basic_column_to_users')}
+  file_dir = "#{dir}/#{file}"
+
+  inject_into_file file_dir, after: "def change\n" do
+    <<~RUBY
+      add_column :users, :open_id, :string
+      add_column :users, :avatar, :string
+      add_column :users, :nickname, :string
+      add_column :users, :phone_number, :string
+      add_column :users, :language, :string
+      add_column :users, :gender, :string
+      add_column :users, :city, :string
+      add_column :users, :province, :string
+      add_column :users, :region, :string
+      add_column :users, :country, :string
+      add_column :users, :is_admin, :boolean, default: false
+    RUBY
+  end
+
+  # App controller
+  ########################################
+  run 'rm app/controllers/application_controller.rb'
+  file 'app/controllers/application_controller.rb', <<~RUBY
+    class ApplicationController < ActionController::Base
+    #{  "protect_from_forgery with: :exception\n" if Rails.version < "5.2"}  before_action :authenticate_user!
+    end
+  RUBY
+
+  # migrate + devise views
+  ########################################
+  rails_command 'db:migrate'
+  generate('devise:views')
+
+  # Pages Controller
+  ########################################
+  # run 'rm app/controllers/pages_controller.rb'
+  # file 'app/controllers/pages_controller.rb', <<~RUBY
+  #   class PagesController < ApplicationController
+  #     skip_before_action :authenticate_user!, only: [ :home ]
+
+  #     def home
+  #     end
+  #   end
+  # RUBY
+
+  # Environments
+  ########################################
+  environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: 'development'
+  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
+  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'staging'
+
+  # Webpacker / Yarn
+  ########################################
+  # run 'yarn add popper.js jquery bootstrap'
+  append_file 'app/javascript/packs/application.js', <<~JS
+
+
+    // ----------------------------------------------------
+    // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
+    // WRITE YOUR OWN JS STARTING FROM HERE 👇
+    // ----------------------------------------------------
+
+    // External imports
+    // import "bootstrap";
+
+    // Internal imports, e.g:
+    // import { initSelect2 } from '../components/init_select2';
+
+    document.addEventListener('turbolinks:load', () => {
+      // Call your functions here, e.g:
+      // initSelect2();
+    });
+  JS
+
+  inject_into_file 'config/webpack/environment.js', before: 'module.exports' do
+    <<~JS
+      const webpack = require('webpack');
+      // Preventing Babel from transpiling NodeModules packages
+      environment.loaders.delete('nodeModules');
+      // Bootstrap 4 has a dependency over jQuery & Popper.js:
+      environment.plugins.prepend('Provide',
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          Popper: ['popper.js', 'default']
+        })
+      );
+    JS
+  end
+
+  # Dotenv
+  ########################################
+  run 'touch .env'
+
+  # Rubocop
+  ########################################
+  run 'curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml'
+
+  # Git
+  ########################################
+  git add: '.'
+  git commit: "-m 'Initial commit with devise template from https://github.com/lewagon/rails-templates'"
+
+  # Fix puma config
+  gsub_file('config/puma.rb', 'pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }', '# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }')
+
+  # DOCKERFILE, PROCFILE, APP.JSON
+end
