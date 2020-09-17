6670988c8b54e5819bd3c4626ce7d59508798c0d
Agnes Selvita <a.selvita@gmail.com>
2020-09-17 20:14:35 +0700

    fix gsub_file

--- wxmp-no-webpack.rb
+++ wxmp-no-webpack.rb
@@ -39,9 +39,9 @@ end
 
 gsub_file 'Gemfile', "source 'https://rubygems.org'", "source 'https://gems.ruby-china.com'"
 # gsub_file('Gemfile', /# gem 'redis'/, "gem 'redis'")
-gsub_file('Gemfile', /gem 'jbuilder'/, "# gem 'jbuilder'")
-gsub_file('Gemfile', /gem 'webpacker'/, "# gem 'webpacker'")
-gsub_file('Gemfile', /gem 'turbolinks'/, "# gem 'turbolinks'")
+gsub_file 'Gemfile', "gem 'jbuilder'", "# gem 'jbuilder'"
+gsub_file 'Gemfile', "gem 'webpacker'", "# gem 'webpacker'"
+gsub_file 'Gemfile', "gem 'turbolinks'", "# gem 'turbolinks'"
 
 # Dev environment
 ########################################
